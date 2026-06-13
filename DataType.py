6dc5bde5e6cc3b78a2be96a20f9373160fa723e6
vishalbaliphadi-rgb <vishal.baliphadi@gmail.com>
2026-06-13 10:43:13 +0530

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,78 @@
+#a = 10
+#print (a) # Value
+#print (type(a)) # type
+#print (id(a)) # memory address
+
+# in below code memory address is same allocated to both a and b
+#a = 10
+#b = 10
+
+#print (id(a)) 
+#print (id(b))
+
+# in below code differnt memory addesses are allocated to a and b
+#a = [10]
+#b = [10]
+
+#print (id(a))
+#print (id(b))
+
+#a = 10
+#b = 10
+
+#print (id(a) == id(b) ) # output is "True" as datatypes for both variables are same
+
+
+#a = 30
+#b = 20 
+
+#addition = a + b
+#print (addition) # addition of 2 numbers
+
+#subtraction = a - b
+#print (subtraction) # subtraction
+
+#multiplication = a*b
+#print (multiplication) # multiplication
+
+#division = a/b 
+#print (division) # division
+
+#x = input ("Enter number: ")
+#print (type(x)) #  output is Enter number: as it returns string datatype
+
+
+# pgm to take user's name and age to display below
+#data = {"Hello" + " Vishal" , "You will turn 39+1 next year"} 
+#print(data)
+
+print ("10" + "20") # output 1020 it consider values as string
+print (10 + 20) # output 30 it considers values as int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
