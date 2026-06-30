2bd0671a955e18924473b2799c3c991a6f9a86e0
vishalbaliphadi-rgb <vishal.baliphadi@gmail.com>
2026-06-30 11:10:15 +0530

    Add files via upload

---
+++ MultiplicationTable.py
@@ -0,0 +1,8 @@
+print ("====multiplication table======================")
+print ("Enter number:") # Enter number for which need table
+a = int(input())
+i1 = 1
+while (i1 < 10):
+    i1 += 1
+    b = a * i1
+    print ("Multiplication table:", b)
