f1ea5e956f9989b360afeb63004540a7b98bec54
vishalbaliphadi-rgb <vishal.baliphadi@gmail.com>
2026-06-30 10:16:15 +0530

    Add files via upload

---
+++ SquareNcube.py
@@ -0,0 +1,12 @@
+print ("Enter number:" )
+no = int(input())
+Ans = no * no # for square of number
+
+print ("Square of number:", Ans)
+
+print ("============================Cube==============================")
+
+print ("Enter Number:")
+No1 = int(input())
+Ans1 = No1*No1*No1
+print ("Cube of the Number is:", Ans1)
