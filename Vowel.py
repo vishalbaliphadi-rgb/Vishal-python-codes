1b3d6eaa9ff0a94bf52648435e629c4ee93d1197
vishalbaliphadi-rgb <vishal.baliphadi@gmail.com>
2026-07-09 20:05:28 +0530

    Add files via upload

---
+++ Vowel.py
@@ -0,0 +1,7 @@
+print ("Enter Alphabet:")
+S = str(input())
+
+if S == ("a" or "A" or "e" or "E" or "i" or "I" or "o" or "O" or "u" or "U"):
+    print ("It's Vowel")
+else:
+    print ("it's consonant")
